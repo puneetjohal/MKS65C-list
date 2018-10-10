a6ee24f9840f9146bb783854dead6548aacf2c64
Puneet <pjohal@stuy.edu>
2018-10-10 04:04:16 -0400

    testing

--- linkedlist.c
+++ linkedlist.c
@@ -16,25 +16,27 @@ struct node* insert_front(struct node* pointer, int i){
   struct node newHead = {i, pointer};
   struct node* newHeadP = &newHead;
   return newHeadP;
-} //returns a
+} //returns segmentation fault?
 
-/*
 //goes through each node in the list and frees memory
 //returns pointer to the beginning of the list (NULL)
 struct node* free_list(struct node* pointer){
   while (pointer){
-
+    nextnode = pointer->next;
+    free(pointer);
+    pointer = nextnode;
   }
-} */
+}
 
 //testing
 int main(){
+  struct node *p = (struct node*)malloc(sizeof(struct node));
   struct node head = {0, NULL};
-  struct node* p = &head;
-  struct node* l = insert_front(p, 1);
+  p = &head;
+  //p = insert_front(p, 1);
   //p = insert_front(p, 2);
   //p = insert_front(p, 3);
-  print_list(l);
+  print_list(p);
   printf("I work up to here\n");
   return 0;
 }
