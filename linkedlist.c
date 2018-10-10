eead28009600c359555a16a1e426b037b80309ef
Puneet <pjohal@stuy.edu>
2018-10-10 03:46:53 -0400

    testing

--- linkedlist.c
+++ linkedlist.c
@@ -16,16 +16,25 @@ struct node* insert_front(struct node* pointer, int i){
   struct node newHead = {i, pointer};
   struct node* newHeadP = &newHead;
   return newHeadP;
-}
+} //returns a
+
+/*
+//goes through each node in the list and frees memory
+//returns pointer to the beginning of the list (NULL)
+struct node* free_list(struct node* pointer){
+  while (pointer){
+
+  }
+} */
 
 //testing
 int main(){
   struct node head = {0, NULL};
   struct node* p = &head;
-  p = insert_front(p, 1);
+  struct node* l = insert_front(p, 1);
   //p = insert_front(p, 2);
   //p = insert_front(p, 3);
-  print_list(p);
+  print_list(l);
   printf("I work up to here\n");
   return 0;
 }
