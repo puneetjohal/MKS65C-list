4d6d7bd84fa69c44982bd03e7fcc9dee3742d637
Puneet <pjohal@stuy.edu>
2018-10-10 21:46:27 -0400

    freelist not quite there yet

--- linkedlist.c
+++ linkedlist.c
@@ -37,7 +37,7 @@ struct node* free_list(struct node* pointer){
 //testing
 int main(){
   struct node *p = (struct node*)malloc(sizeof(struct node)); //allocates memory
-  p = &{1, NULL};
+  struct node head = {1, NULL};
   printf("Original: ");
   print_list(p);
   printf("Inserting values... \n");
